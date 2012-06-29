ca4a3591ee4e01bc7d276532adb02e691ff34c6d
Tom MacWright <tom@macwright.org>
2012-06-29 13:19:03 -0400

    Update modestmaps and simplestyle factory to add print and touch capability

--- ext/simplestyle_factory.js
+++ ext/simplestyle_factory.js
@@ -13,22 +13,20 @@ function simplestyle_factory(feature) {
     var color = fp['marker-color'] || '7e7e7e';
     color = color.replace('#', '');
 
-    var d = document.createElement('div');
+    var d = document.createElement('img');
+    d.width = sizes[size][0];
+    d.height = sizes[size][1];
     d.className = 'simplestyle-marker';
+    d.alt = fp.title || '';
+    d.src = (simplestyle_factory.baseurl || 'http://a.tiles.mapbox.com/v3/marker/') +
+      'pin-' + size[0] + symbol + '+' + color + '.png';
+
     var ds = d.style;
     ds.position = 'absolute';
-    ds.width = sizes[size][0] + 'px';
-    ds.height = (sizes[size][1] * 0.75) + 'px';
+    ds.clip = 'rect(auto auto ' + (sizes[size][1] * 0.75) + 'px auto)';
     ds.marginTop = -((sizes[size][1]) / 2) + 'px';
     ds.marginLeft = -(sizes[size][0] / 2) + 'px';
     ds.cursor = 'pointer';
-
-    ds.backgroundImage = 'url(' +
-      (simplestyle_factory.baseurl || 'http://a.tiles.mapbox.com/v3/marker/') +
-      'pin-' + size[0] + symbol + '+' + color + '.png)';
-
-    ds.textIndent = '-10000px';
-    d.innerHTML = fp.title || '';
 
     return d;
 }
