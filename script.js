283265df92a61e8da708063a6df360904c8dbccc
Ansis Brammanis <ansis.brammanis@gmail.com>
2012-07-26 11:50:22 -0400

    Now using mapbox.js

--- script.js
+++ script.js
@@ -1,338 +1,40 @@
-(function(root) {
-    var Map = {},
-        layers;
+// Create and load map
+$('#map').mapbox('examples.map-8j8lv902', function(map, tilejson) {
 
-    Map = function(el, l, callback) {
-        var map = this;
-        wax.tilejson(l.api, function(t) {
-            var handlers = [
-                new MM.DragHandler(),
-                new MM.DoubleClickHandler(),
-                new MM.TouchHandler()
-            ];
-            if ($.inArray('zoomwheel', l.features) >= 0) {
-                handlers.push(new MM.MouseWheelHandler());
-            }
-            if ($.inArray('static', l.features) >= 0) {
-                handlers = null;
-            }
+    tilejson = tilejson[0]; // Remove when upgrading to 0.6.3
+    
+    map.setZoomRange(0, 15);
 
-            MM_map = new MM.Map(el, new wax.mm.connector(t), null, handlers);
-            setCenterOffset(MM_map, {
-                lat: (l.center) ? l.center.lat : t.center[1],
-                lon: (l.center) ? l.center.lon : t.center[0]
-            }, (l.center) ? l.center.zoom : t.center[2]);
-
-            if (l.zoomRange) {
-                MM_map.setZoomRange(l.zoomRange[0], l.zoomRange[1]);
-            } else {
-                MM_map.setZoomRange(t.minzoom, t.maxzoom);
-            }
-
-            wax.mm.attribution(MM_map, t).appendTo(MM_map.parent);
-
-            for (var i = 0; i < l.features.length; i++) {
-                switch(l.features[i]) {
-                    case 'zoompan':
-                        wax.mm.zoomer(MM_map).appendTo(MM_map.parent);
-                        break;
-                    case 'zoombox':
-                        wax.mm.zoombox(MM_map);
-                        break;
-                    case 'legend':
-                        MM_map.legend = wax.mm.legend(MM_map, t).appendTo(MM_map.parent);
-                        break;
-                    case 'bwdetect':
-                        wax.mm.bwdetect(MM_map);
-                        break;
-                    case 'share':
-                        wax.mm.share(MM_map, t).appendTo($('body')[0]);
-                        break;
-                    case 'tooltips':
-                        MM_map.interaction = wax.mm.interaction()
-                            .map(MM_map)
-                            .tilejson(t)
-                            .on(wax.tooltip()
-                                .parent(MM_map.parent)
-                                .events()
-                            );
-                        break;
-                    case 'movetips':
-                        MM_map.interaction = wax.mm.interaction()
-                            .map(MM_map)
-                            .tilejson(t)
-                            .on(wax.movetip()
-                                .parent(MM_map.parent)
-                                .events()
-                            );
-                        break;
-                }
-            }
-            Map.bootstrap(l);
-            picks.start(t.data[0]);
-            if (callback && typeof(callback) == 'function') callback(t);
-        });
-        return Map;
-    };
-
-    Map.layers = function(x) {
-        if (!arguments.length) return layers;
-        layers = x;
-        return Map;
-    };
-
-    Map.layerGroups = [];
-
-    Map.setOverlay = function(id) {
-
-        if (id) {
-            if (!layers[id]) throw new Error('overlay with id ' + id + ' not found');
-            var l = layers[id];
-
-            l.group = l.group || 0;
-
-            if (l.api) {
-                Map.layerGroups[l.group] = {
-                    id: id,
-                    api: l.api
-                };
-            }
-        }
-
-        if ((l && l.api) || (!l && Map.layerGroups.length > 0)) {
-            var compositedLayers = function() {
-                var layerIds = [];
-                $.each(Map.layerGroups, function(index, layer) {
-                    if (layer && layer.api) {
-                        layerIds.push(layer.api.match(/v\d\/(.*?).jsonp/)[1]);
-                        $('[href="#' + layer.id + '"]').addClass('active');
-                    }
-                });
-                return 'http://a.tiles.mapbox.com/v3/' + layerIds.join(',') + '.jsonp';
-            };
-
-            wax.tilejson(compositedLayers(), function(t) {
-                var level = (l && l.level === 'base') ? 0 : 1;
-
-                try {
-                    MM_map.setLayerAt(level, new wax.mm.connector(t));
-                } catch (e) {
-                    MM_map.insertLayerAt(level, new wax.mm.connector(t));
-                }
-                if (MM_map.interaction) MM_map.interaction.map(MM_map).tilejson(t);
-                if (MM_map.legend) {
-                    MM_map.legend.content(t);
-                }
-            });
-        }
-
-        if (l && l.center) {
-            var lat = l.center.lat || MM_map.getCenter().lat,
-                lon = l.center.lon || MM_map.getCenter().lon,
-                zoom = l.center.zoom || MM_map.getZoom();
-
-            if (l.center.ease > 0) {
-                MM_map.easey = easey().map(MM_map)
-                    .to(MM_map.locationCoordinate({ lat: lat, lon: lon })
-                    .zoomTo(zoom)).run(l.center.ease);
-            } else {
-                setCenterOffset(MM_map, { lat: lat, lon: lon }, zoom);
-            }
-        }
-    };
-
-    Map.removeOverlay = function(id) {
-
-        if (!layers[id]) throw new Error('overlay with id ' + id + ' not found');
-        var l = layers[id];
-
-        l.group = l.group || 0;
-        delete Map.layerGroups[l.group];
-
-        if (cleanArray(Map.layerGroups).length > 0) {
-            Map.setOverlay();
-        } else {
-            var level = (l.level === 'base') ? 0 : 1;
-            MM_map.removeLayerAt(level);
-            if (MM_map.legend) MM_map.legend.content(' ');
-            if (MM_map.interaction) MM_map.interaction.remove();
-        }
-    };
-
-    Map.parseHash = function() {
-        var pattern = /(?:#([^\?]*))?(?:\?(.*))?$/,
-            components = window.location.href.match(pattern);
-
-        if (components && components[2] === 'embed') {
-            $('body').removeClass().addClass('embed');
-            window.location.replace(window.location.href.split('?')[0]);
-        }
-
-        if (components && components[1]) {
-            var hash = components[1];
-            if (hash.substr(0, 1) === '/') hash = hash.substring(1);
-            if (hash.substr(hash.length - 1, 1) === '/') hash = hash.substr(0, hash.length - 1);
-
-            ids = decodeURIComponent(hash).split('/');
+    // Set title and description from tilejson
+    document.title = tilejson.name;
+    $('h1.map-title').text(tilejson.name);
+    $('p.description').text(tilejson.description);
 
 
-            $.each(ids, function(i, layer) {
-                if (layer !== '-' && layers[layer]) {
-                    Map.layerGroups[i] = {
-                        id: layer,
-                        api: layers[layer].api
-                    };
-                }
-            });
-        }
-        if (cleanArray(Map.layerGroups).length > 0) {
-            Map.setOverlay();
-        } else {
-            if (layers) {
-                $.each(layers, function(key, layer) {
-                    if (layer.initial) Map.setOverlay(key);
-                });
-            }
-        }
-    };
+    var container = $('#markerfilters');
+    $.each(tilejson.markers.markers(), function(index, m) {
+        var s = m.data.properties['marker-symbol'];
 
-    Map.setHash = function() {
-        var hash = [];
+        if (container.find('[href="#' + s + '"]').length) return;
 
-        $.each(Map.layerGroups, function(index, layer) {
-            var id = (layer && layer.id) ? layer.id : '-';
-            hash.push(encodeURIComponent(id));
-        });
-
-        var l = window.location,
-            baseUrl = l.href,
-            state = (hash.length > 0) ? '#/' + hash.join('/') : '';
-
-        if (baseUrl.indexOf('?') >= 0) baseUrl = baseUrl.split('?')[0];
-        if (baseUrl.indexOf('#') >= 0) baseUrl = baseUrl.split('#')[0];
-
-        l.replace(baseUrl + state);
-
-        // Update share urls
-        var webpage = l.href;
-        var embed = (l.hash) ? l.href + '?embed' : l.href + '#/?embed';
-
-        $('.wax-share textarea.embed').val(
-            '<iframe width="500" height="300" frameBorder="0" src="{embed}"></iframe>'
-            .replace('{embed}', embed));
-        $('.wax-share a.twitter').attr('href', 'http://twitter.com/intent/tweet?status='
-            + encodeURIComponent(document.title + ' (' + webpage + ')'));
-        $('.wax-share a.facebook').attr('href', 'https://www.facebook.com/sharer.php?u='
-            + encodeURIComponent(webpage) + '&t=' + encodeURIComponent(document.title));
-    };
-
-    root.Map = Map;
-
-})(this);
-
-// Bind the geocoder functionality to any div with the format
-//
-//     <div data-control='geocode' id="search">
-//        <form class="geocode">
-//          <input placeholder='Search for an address' type="text">
-//          <input type='submit' />
-//          <div id='geocode-error'></div>
-//        </form>
-//      </div>
-//
-function bindGeocoder() {
-    $('[data-control="geocode"] form').submit(function(e) {
-        var m = $('[data-control="geocode"]').attr('data-map');
-        // If this doesn't explicitly name the layer it should affect,
-        // use the first layer in MB.maps
-        e.preventDefault();
-        geocode($('input[type=text]', this).val(), m);
+        var el = $(document.createElement('a'))
+            .addClass('markerfilter')
+            .attr('href', '#' + s)
+            .css('background-image', 'url(http://a.tiles.mapbox.com/v3/marker/pin-l-'+s+'+000000.png)')
+            .bind('click', filter);
+        container.append(el);
     });
-    var geocode = function(query, m) {
-        query = encodeURIComponent(query);
-        $('form.geocode').addClass('loading');
-        reqwest({
-            url: 'http://open.mapquestapi.com/nominatim/v1/search?format=json&&limit=1&q=' + query,
-            type: 'jsonp',
-            jsonpCallback: 'json_callback',
-            success: function (r) {
-                r = r[0];
 
-                if (MM_map.geocodeLayer) {
-                    MM_map.geocodeLayer.removeAllMarkers();
-                }
 
-                $('form.geocode').removeClass('loading');
+    $('[href="#all"]').bind('click', filter);
 
-                if (r === undefined) {
-                    $('#geocode-error').text('This address cannot be found.').fadeIn('fast');
-                } else {
-                    $('#geocode-error').hide();
-                    MM_map.setExtent([
-                        { lat: r.boundingbox[1], lon: r.boundingbox[2] },
-                        { lat: r.boundingbox[0], lon: r.boundingbox[3] }
-                    ]);
 
-                    if (MM_map.getZoom() === MM_map.coordLimits[1].zoom) {
-                        var point = { 'type': 'FeatureCollection',
-                            'features': [{ 'type': 'Feature',
-                            'geometry': { 'type': 'Point','coordinates': [r.lon, r.lat] },
-                            'properties': {}
-                        }]};
-
-                        setCenterOffset(MM_map, { lat: r.lat, lon: r.lon });
-                    }
-                }
-            }
+    function filter(e) {
+        container.find('a').removeClass('selected');
+        var id = $(this).addClass('selected').attr('href').replace('#', '');
+        tilejson.markers.filter(function(feature) {
+            return feature.properties['marker-symbol'] == id || id == 'all';
         });
-        var attribution = 'Search by <a href="http://developer.mapquest.com/web/products/open">MapQuest Open</a>';
-        if ($('.wax-attribution').html().indexOf(attribution) < 0) {
-            $('.wax-attribution').append(' - ' + attribution);
-        }
-    };
-}
-
-function cleanArray(actual){
-    var newArray = new Array();
-    for(var i = 0; i<actual.length; i++){
-        if (actual[i]){
-            newArray.push(actual[i]);
-        }
+        return false;
     }
-    return newArray;
-}
-
-Map.bootstrap = function(l) {
-
-    $('body').on('click.map', '[data-control="layer"]', function(e) {
-        var $this = $(this),
-            id = $this.attr('href');
-        id = id.replace(/.*(?=#[^\s]+$)/, '').slice(1);
-        var m = $('[data-control="geocode"]').attr('data-map') || 'main';
-        e.preventDefault();
-        if($this.hasClass('active')) {
-            if(Map.layers()[id].toggle) {
-                $('[data-control="layer"]').removeClass('active');
-                window[m].removeOverlay(id);
-            }
-        } else {
-            $('[data-control="layer"]').removeClass('active');
-            window[m].setOverlay(id);
-        }
-        if ($.inArray('layerHash', l.features) >= 0) Map.setHash();
-    });
-
-    bindGeocoder();
-    Map.parseHash();
-};
-
-// Set center on map while offsetting to compensate for drawer if open.
-var setCenterOffset = function(map, location, zoom) {
-    if (zoom)
-        map.setZoom(zoom);
-    var drawerWidth = 320;
-    var point = map.locationPoint(location);
-    point.x += drawerWidth / 2;
-    location = map.pointLocation(point);
-    map.setCenter(location);
-}
+});
