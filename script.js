786b2d361553cfff36d2fe6608ff18f38625f590
Justin R. Miller <incanus@codesorcery.net>
2012-12-11 11:29:16 -0500

    fix map / update to new map

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Create and load map
-$('#map').mapbox('examples.map-8j8lv902', function(map, tilejson) {
+$('#map').mapbox('examples.map-zr0njcqy', function(map, tilejson) {
 
     map.setZoomRange(0, 15);
 
